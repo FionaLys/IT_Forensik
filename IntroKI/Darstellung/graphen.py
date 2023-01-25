fe3203aa90303b40b2ecf5e7ccaef90101141964
FionaLayes <104022317+FionaLys@users.noreply.github.com>
2023-01-25 10:40:35 +0100

    Delete IntroKI directory

--- IntroKI/Darstellung/graphen.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plotting(df, y_label):
-    plt.plt(df)
-    plt.ylabel(y_label)
-    plt.show()
--- IntroKI/Übungsblatt_2/aufgabe_10.py
+++
@@ -1,9 +0,0 @@
-# E(w) = 6w^2 + 3w - 5
-# E'(w) = 12w + 3
-# eta = 0,1
-
-def loss(w1):
-    # w^(t+1) = w(t) - eta * E(w(t))
-    eta = 0.1
-    E = 6*w1**2+3*w1-5
-    return (w1 - eta * E)
